0a92930b971d9465ab56069bee35478aeba012f2
Rudi Starcevic <ooly.me@gmail.com>
2017-12-19 15:16:50 +0800

    Updates

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,6 +1,8 @@
 job('NodeJS Example') {
-    scm git('https://github.com/rudijs/docker-demo-web-app.git') { node -> 
-        node / gitConfigName('DSL User')
-        node / gitConfigEmail('rudi.starcevic@viseo.com')
+    scm {
+        git('https://github.com/rudijs/docker-demo-web-app.git') { node -> 
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('rudi.starcevic@viseo.com')
+        }
     }
 }
